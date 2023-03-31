f6ac4f475c10f912ac44ecc184776843d0623169
sol gudmin <115568958+schnappiiiiiiii@users.noreply.github.com>
2023-03-31 17:57:33 -0500

    halfway there

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -8,17 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var assignmentItems = [AssignmentItem(id: <#UUID#>, priority: "High", description: "", dueDate: Date()), AssignmentItem(id: <#UUID#>, priority: "Medium", description: "", dueDate: Date()), AssignmentItem(id: <#UUID#>, priority: "Low", description: "", dueDate: Date())]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            List{
+                ForEach(assignmentItems) {item in
+                    HStack{
+                        VStack(alignment: .leading) {
+                            Text(item.priority)
+                                .font(.headline)
+                            Text(item.description)
+                        }
+                        Spacer()
+                        Text(item.dueDate, style: .date)
+                    }
+                }
+                .onMove { indices, newOffset in
+                    assignmentItems.move(fromOffsets: indices, toOffset: newOffset)
+                }
+                .onDelete { indexSet in
+                    assignmentItems.remove(atOffsets: indexSet)
+                }
+                .navigationBarItems(leading: EditButton())
+            }
         }
-        .padding()
     }
 }
-
+struct AssignmentItem: Identifiable{
+    var id: UUID
+    var priority = String()
+    var description = String()
+    var dueDate = Date()
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
